2463b524d0a3713d18cfab5af8221a2b7c84d6a6
Vincent Vanlerberghe <vincentvanlerberghe@Vincents-MacBook-Pro.local>
2025-01-21 22:02:07 +0100

    module 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://verdant-cendol-a61149.netlify.app/"
-
+  site: "https://verdant-cendol-a61149.netlify.app/",
+  integrations: [preact()]
 });
